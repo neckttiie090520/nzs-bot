59fe1d7d908083a6d51a6f779966f20d4bccfec4
neckttiie090520 <88713733+neckttiie090520@users.noreply.github.com>
2024-07-02 23:35:44 +0700

    myserver.py

--- myserver.py
+++ myserver.py
@@ -8,8 +8,11 @@ def home():
     return "Server is running!"
 
 def run():
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
-    def server_on():
-        t = Thread(target=run)
-        t.start()
+def server_on():
+    t = Thread(target=run)
+    t.start()
+
+def server_off():
+    print("Server is off")
